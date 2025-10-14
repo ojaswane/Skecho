d6c5580ce5962d7aa3b68455bd841b3ce071ba01
ojas wane <ojaswane79@gmail.com>
2025-10-14 17:28:12 +0530

    app updated

--- src/loading.tsx
+++ src/loading.tsx
@@ -3,7 +3,7 @@ import { BarLoader } from 'react-spinners'
 const loading = () => {
     return (
         <div className=' flex justify-center items-center h-screen'>
-            <BarLoader color="#36d7b7" width={200} />
+            <BarLoader color="white" width={200} />
         </div>
     )
 }
