ec65b981524a77bf82fc3343423a7ca316dc64f8
ojas wane <ojaswane79@gmail.com>
2025-10-15 17:12:55 +0530

    rtk setup

--- src/redux/provider.ts
+++ src/redux/provider.ts
@@ -1,19 +1,22 @@
 // this is the main redux provider for the app
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { slices } from './slices'
+// import { middleware } from '@/middleware'
+export type Rootstate = ReturnType<typeof store.getState>
 
 const rootReducer = combineReducers({
     ...slices
 })
 
-export function makeStore() {
+export function makeStore(preloadedState?: Partial<Rootstate>): ReturnType<typeof configureStore> {
     return (
         configureStore({
             reducer: rootReducer,
+            middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
+            
         })
     )
 }
 
 
 export const store = makeStore()
-    
