0574b61c6d41379511dedb25e46df5c7f7ee6366
ojas wane <ojaswane79@gmail.com>
2025-10-31 12:01:51 +0530

    added new files

---
+++ src/app/(protected)/workspace/[id]/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+type props = { 
+    children: React.ReactNode
+}
+const layout = ({children}: props) => {
+  return (
+    <>
+        <div>
+            
+        </div>
+    </>
+  )
+}
+
+export default layout
