14f8ce2f8e9674edbcf8218c3884e7db436e15c4
ojas wane <ojaswane79@gmail.com>
2025-11-01 17:10:00 +0530

    Added breadcrumb in workspace

--- src/components/canvas/Navbar.tsx
+++ src/components/canvas/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react"
+import { Breadcrumb } from "../ui/breadcrumb"
+
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center px-8 py-4 mt-2 mx-2 backdrop-blur-xl bg-white/70 dark:bg-neutral-900/50 rounded-2xl shadow-sm">
       
       {/* Left Side - Breadcrumb */}
       <div className="flex items-center space-x-2">
-            
+            <Breadcrumb/>
       </div>
   
       {/* Center - Tabs or Title */}
