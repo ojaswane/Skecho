6ebb5fedcef8c9f00cf76cd0c981f6c2564822ce
ojas wane <ojaswane79@gmail.com>
2025-11-06 21:42:14 +0530

    Updated

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,6 +1,12 @@
 // this is a react render component for the canvas area
 import {useState , useRef , useEffect} from 'react'
+import { shape , ShapeType } from '../../../lib/type'
 const Canvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [shapes , useShapes] = useState<shape[]>([])
+  const [selectted , setSelected] =useState<shape | null>(null)
+  const [isDragging , setIsDragging] = useState<{offsetX : number, offsetY : number }| null>(null)
+  
   
   return (
     <>
