59d163bd42e09623916a6758106ef6155a49939f
ojas wane <ojaswane79@gmail.com>
2025-11-08 23:38:15 +0530

    Added canvas (phase 1 , just drag and create objects)

--- src/app/(protected)/workspace/[id]/layout.tsx
+++ src/app/(protected)/workspace/[id]/layout.tsx
@@ -1,17 +1,21 @@
 import Navbar from '@/components/canvas/Navbar'
 import React from 'react'
 import Toolbar from '@/components/canvas/Toolbar'
+import CanvasBoard from '@/components/canvas/Canvas'
 
-type props = { 
-    children: React.ReactNode
+type props = {
+  children: React.ReactNode
 }
-const layout = ({children}: props) => {
+const layout = ({ children }: props) => {
   return (
     <>
-        <div className=''>
-            <Navbar />
-            <Toolbar />
+      <main className="flex flex-col h-screen">
+        <Navbar />
+        <div className="flex-1 flex justify-center items-center">
+          <CanvasBoard />
         </div>
+        <Toolbar />
+      </main>
     </>
   )
 }
