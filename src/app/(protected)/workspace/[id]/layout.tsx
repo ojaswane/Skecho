c8edcea37699b91c10102d590aa7cbc8ee599f93
ojas wane <ojaswane79@gmail.com>
2025-12-07 14:31:09 +0530

    Updated

--- src/app/(protected)/workspace/[id]/layout.tsx
+++ src/app/(protected)/workspace/[id]/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const theme = useCanvasStore((s) => s.theme);
 
   return (
-    <main className="flex flex-col h-screen dark:bg-[#1a1a1a]  bg-white dark:text-white text-black">
+    <main className="flex flex-col h-screen dark:bg-[#1a1a1a] bg-white dark:text-white text-black">
       <Navbar />
 
       <div className="flex-1 flex justify-center items-center">
--- src/components/canvas/Navbar.tsx
+++ src/components/canvas/Navbar.tsx
@@ -4,11 +4,11 @@ import Breadcrumbb from "../ui/breadcrumb/workspace/Breadcrumb"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center px-8 py-4 mt-2 mx-2 backdrop-blur-xl bg-white/70 dark:bg-neutral-900/50 rounded-2xl shadow-sm">
+    <div className="flex justify-between items-center px-8 py-4 mt-2 mx-2 backdrop-blur-xl border-2 dark:bg-white/5  dark:border-white/10 dark:text-neutral-300 bg-black/30  border-black/20 text-black rounded-2xl shadow-sm">
 
       {/* Left Side - Breadcrumb */}
       <div className="flex items-center space-x-2">
-        <Breadcrumbb/>
+        <Breadcrumbb />
       </div>
 
       {/* Right Side - Credits / Export / Profile */}
