cea80126f9f34b2b39ddd49aadef79c93e4ab7e1
ojas wane <ojaswane79@gmail.com>
2025-12-07 11:29:40 +0530

    Updated the canvas with theme

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import { useRef } from 'react';
 import { Canvas } from 'fabric';
-const CanvasRender = () => {
+const CanvasRender = ({ theme }: { theme: "light" | "dark" }) => {
   const canvasRef = useRef(null);
   const [canvas, setCanvas] = React.useState<any>(null);
   useEffect(() => {
@@ -10,7 +10,7 @@ const CanvasRender = () => {
       const initCanvas = new Canvas(canvasRef.current, {
         width: 800,
         height: 600,
-        backgroundColor: '#ffffff',
+        backgroundColor: theme === "dark" ? "#1a1a1a" : "#ffffff",
       });
       initCanvas.renderAll();
       setCanvas(initCanvas);
@@ -19,8 +19,17 @@ const CanvasRender = () => {
         initCanvas.dispose();
       }
     }
-
   }, []);
+
+
+  useEffect(() => {
+    if (!canvas) return;
+
+    canvas.setBackgroundColor(
+      theme === "dark" ? "#1a1a1a" : "#ffffff",
+      () => canvas.renderAll()
+    );
+  }, [theme, canvas]);
   return (
     <div>
       <canvas ref={canvasRef} id='canvas' />
