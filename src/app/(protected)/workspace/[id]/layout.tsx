b274976dda4b800086f5963eeff70867aea75dc2
ojas wane <ojaswane79@gmail.com>
2025-12-07 11:37:13 +0530

    Canvas updated

--- src/app/(protected)/workspace/[id]/layout.tsx
+++ src/app/(protected)/workspace/[id]/layout.tsx
@@ -1,23 +1,24 @@
-import Navbar from '@/components/canvas/Navbar'
-import React from 'react'
-import Toolbar from '@/components/canvas/Toolbar'
-import CanvasBoard from '@/components/canvas/Canvas'
+"use client";
 
-type props = {
-  children: React.ReactNode
-}
-const layout = ({ children }: props) => {
+import Navbar from "@/components/canvas/Navbar";
+import Toolbar from "@/components/canvas/Toolbar";
+import CanvasBoard from "@/components/canvas/Canvas";
+import { useCanvasStore } from "../../../../../lib/store/canvasStore"; // if using zustand
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const theme = useCanvasStore((s) => s.theme); // "light" | "dark"
+
   return (
-    <>
-      <main className="flex flex-col h-screen">
-        <Navbar />
-        <div className="flex-1 flex justify-center items-center">
-          <CanvasBoard />
-        </div>
-        <Toolbar />
-      </main>
-    </>
-  )
-}
+    <main className="flex flex-col h-screen">
+      <Navbar />
 
-export default layout
+      <div className="flex-1 flex justify-center items-center">
+        <CanvasBoard theme={theme} />
+      </div>
+
+      <Toolbar />
+    </main>
+  );
+};
+
+export default Layout;
