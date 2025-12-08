c9c13e4fc3234a92c935fa2f4c27df436d90e18c
ojas wane <ojaswane79@gmail.com>
2025-12-08 20:15:30 +0530

    Updated ui

--- src/app/(protected)/workspace/[id]/layout.tsx
+++ src/app/(protected)/workspace/[id]/layout.tsx
@@ -3,22 +3,34 @@
 import Navbar from "@/components/canvas/Navbar";
 import Toolbar from "@/components/canvas/Toolbar";
 import CanvasBoard from "@/components/canvas/Canvas";
-import { useCanvasStore } from "../../../../../lib/store/canvasStore";
 import Objectdetails from "@/components/canvas/Objectdetails";
+import { useCanvasStore } from "../../../../../lib/store/canvasStore";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = () => {
   const theme = useCanvasStore((s) => s.theme);
 
   return (
     <main className="flex flex-col h-screen dark:bg-[#1a1a1a] bg-white dark:text-white text-black">
+
+      {/* TOP NAV */}
       <Navbar />
 
-      <div className="flex-1 flex justify-center items-center">
-        {/* This is the objects details */}
-        <Objectdetails />
-        <CanvasBoard theme={theme} />
+      {/* MAIN EDITOR AREA */}
+      <div className="flex flex-1 overflow-hidden">
+
+        {/* CANVAS AREA */}
+        <div className="flex-1 overflow-hidden">
+          <CanvasBoard theme={theme} />
+        </div>
+
+        {/* RIGHT SIDEBAR (properties panel) */}
+        <div className="w-72 border-l border-gray-300 dark:border-gray-700 overflow-y-auto">
+          <Objectdetails />
+        </div>
+
       </div>
 
+      {/* BOTTOM TOOLBAR */}
       <Toolbar />
     </main>
   );
