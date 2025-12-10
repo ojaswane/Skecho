18183d770f8673751a20c0da31c2926d2ad4b26b
ojas wane <ojaswane79@gmail.com>
2025-12-10 09:29:29 +0530

    Added new dependency

---
+++ src/components/canvas/colorpicker.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ColourPickerEditor from './ColourPickerEditor'
+const colorpicker = () => {
+  return (
+    <div>colorpicker</div>
+  )
+}
+
+export default colorpicker
