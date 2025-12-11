5be9ce12294f17313321a8acd9e4d179ebd47ec1
ojas wane <ojaswane79@gmail.com>
2025-12-11 16:56:29 +0530

    Done with the color picker

--- src/components/canvas/Objectdetails.tsx
+++ src/components/canvas/Objectdetails.tsx
@@ -77,11 +77,11 @@ const Objectdetails = () => {
                 <Popover>
                   <PopoverTrigger asChild>
                     <button
-                      className=" w-full h-9 rounded-4xl  border border-white/20 bg-white/20 dark:bg-white/10  cursor-pointer flex items-center px-2 gap-2 "
+                      className=" w-full mt-4 h-9 rounded-4xl  border border-white/20 bg-white/20 dark:bg-white/10  cursor-pointer flex items-center gap-2 "
                     >
                       {/* Color preview circle */}
                       <div
-                        className="w-full h-full p-0 rounded-full "
+                        className="w-full h-full rounded-full "
                         style={{ background: colorSet }}
                       />
                     </button>
