4e930329d729a8f2c296803a635f260681ea5967
ojas wane <ojaswane79@gmail.com>
2025-12-16 16:52:10 +0530

    Adding new features

--- src/components/canvas/Textoptions.tsx
+++ src/components/canvas/Textoptions.tsx
@@ -1,17 +1,36 @@
 'use client'
 import React from 'react'
 import { Combobox } from '../ui/Combobox'
+import { TextAlignStart } from 'lucide-react'
+import { TextAlignEnd } from 'lucide-react'
+import { TextAlignCenter } from 'lucide-react'
 
 const Textoptions = () => {
   return (
-    <div className='-mt-2 flex flex-col text-white'>
-      <label className='uppercase text-[11px] opacity-60 tracking-wide'>
-        Font Family
-      </label>
-      <div>
-        <Combobox />
+    <>
+      <div className='-mt-2 flex flex-col text-white'>
+        <label className='uppercase text-[11px] opacity-60 tracking-wide'>
+          Font Family
+        </label>
+        <div>
+          <Combobox />
+        </div>
       </div>
-    </div>
+
+      {/* text alignment + line spacing */}
+      <div className='mt-3 flex flex-col text-white'>
+        <div>
+          <label className='uppercase text-[11px] opacity-60 tracking-wide'>
+            text align
+          </label>
+
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+
   )
 }
 
