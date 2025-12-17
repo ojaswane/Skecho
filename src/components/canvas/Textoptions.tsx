884806ef9995be82afe4db2a91b02ba5086fcf9e
ojas wane <ojaswane79@gmail.com>
2025-12-17 17:57:19 +0530

    Updated

--- src/components/canvas/Textoptions.tsx
+++ src/components/canvas/Textoptions.tsx
@@ -1,36 +1,60 @@
 'use client'
 import React from 'react'
 import { Combobox } from '../ui/Combobox'
-import { TextAlignStart } from 'lucide-react'
-import { TextAlignEnd } from 'lucide-react'
-import { TextAlignCenter } from 'lucide-react'
+import {
+  TextAlignStart,
+  TextAlignCenter,
+  TextAlignEnd,
+} from 'lucide-react'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
 
 const Textoptions = () => {
   return (
-    <>
-      <div className='-mt-2 flex flex-col text-white'>
-        <label className='uppercase text-[11px] opacity-60 tracking-wide'>
+    <div className="space-y-4">
+
+      {/* Font Family */}
+      <div className="flex flex-col text-white">
+        <label className="uppercase text-[11px] opacity-60 tracking-wide">
           Font Family
         </label>
-        <div>
-          <Combobox />
-        </div>
+        <Combobox />
       </div>
 
-      {/* text alignment + line spacing */}
-      <div className='mt-3 flex flex-col text-white'>
-        <div>
-          <label className='uppercase text-[11px] opacity-60 tracking-wide'>
-            text align
-          </label>
+      {/* Text Alignment */}
+      <div className="flex flex-col text-white">
+        <label className="uppercase text-[11px] opacity-60 tracking-wide mb-1">
+          Text Align
+        </label>
 
-          <div>
-            
-          </div>
-        </div>
+        <Select defaultValue="left">
+          <SelectTrigger className="h-9 bg-zinc-900 border-zinc-700">
+            <SelectValue placeholder="Select alignment" />
+          </SelectTrigger>
+
+          <SelectContent className="bg-zinc-900 border-zinc-700">
+            <SelectItem value="left" className="flex items-center gap-2">
+              <TextAlignStart size={16} />
+            </SelectItem>
+
+            <SelectItem value="center" className="flex items-center gap-2">
+              <TextAlignCenter size={16} />
+            </SelectItem>
+
+            <SelectItem value="right" className="flex items-center gap-2">
+              <TextAlignEnd size={16} />
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-    </>
 
+    </div>
   )
 }
 
