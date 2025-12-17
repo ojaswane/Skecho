d272c72403b2d0fa8afedf2ccd64a7ed08bb6237
ojas wane <ojaswane79@gmail.com>
2025-12-17 18:38:04 +0530

    Done with text alignment

--- src/components/canvas/Textoptions.tsx
+++ src/components/canvas/Textoptions.tsx
@@ -1,5 +1,7 @@
 'use client'
 import React from 'react'
+import { Text, IText, Textbox } from 'fabric'
+import * as fabric from 'fabric'
 import { Combobox } from '../ui/Combobox'
 import {
   TextAlignStart,
@@ -12,10 +14,44 @@ import {
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue,
 } from '../ui/select'
 
+import { useCanvasStore } from '../../../lib/store/canvasStore'
+import { SelectValue } from '@radix-ui/react-select'
+
+type TextAlign = 'left' | 'center' | 'right'
+
+
 const Textoptions = () => {
+  const canvas = useCanvasStore((s) => s.canvas)
+
+  type FabricTextObject = Text | IText | Textbox
+
+  const isFabricTextObject = (obj: unknown): obj is FabricTextObject => {
+    return (
+      typeof obj === 'object' &&
+      obj !== null &&
+      'type' in obj &&
+      (
+        (obj as any).type === 'text' ||
+        (obj as any).type === 'i-text' ||
+        (obj as any).type === 'textbox'
+      )
+    )
+  }
+
+
+  const updateTextAlign = (value: TextAlign) => {
+    if (!canvas) return
+
+    const obj = canvas.getActiveObject()
+    if (!isFabricTextObject(obj)) return
+
+    obj.textAlign = value
+    canvas.requestRenderAll()
+  }
+
+
   return (
     <div className="space-y-4">
 
@@ -33,21 +69,24 @@ const Textoptions = () => {
           Text Align
         </label>
 
-        <Select defaultValue="left">
-          <SelectTrigger className="h-9 bg-zinc-900 border-zinc-700">
-            <SelectValue placeholder="Select alignment" />
+        <Select
+          defaultValue="left"
+          onValueChange={(value) => updateTextAlign(value as TextAlign)}
+        >
+          <SelectTrigger className="h-9 bg-zinc-900 border-zinc-700" >
+            <SelectValue />
           </SelectTrigger>
 
           <SelectContent className="bg-zinc-900 border-zinc-700">
-            <SelectItem value="left" className="flex items-center gap-2">
+            <SelectItem value="left">
               <TextAlignStart size={16} />
             </SelectItem>
 
-            <SelectItem value="center" className="flex items-center gap-2">
+            <SelectItem value="center">
               <TextAlignCenter size={16} />
             </SelectItem>
 
-            <SelectItem value="right" className="flex items-center gap-2">
+            <SelectItem value="right">
               <TextAlignEnd size={16} />
             </SelectItem>
           </SelectContent>
