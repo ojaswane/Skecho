3fec8f8727cb1199079c63e973c94384571a68b0
ojas wane <ojaswane79@gmail.com>
2026-01-21 21:29:36 +0530

    LESSS GOOO , MY AI IS WORKINNGG

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
 import generateRoute from "./routes/generate"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -10,5 +13,5 @@ app.use(express.json())
 app.use("/generate", generateRoute)
 
 app.listen(3001, () => {
-    console.log("Backend running") 
+    console.log("Backend running")
 })
